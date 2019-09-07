f44dfae7598ff78acf4511209b5e188ebbdf5e7f
Fabian Benavides <jfbenavid@hotmail.com>
2019-09-07 17:26:32 -0500

    changed app.js to use hooks

--- src/components/app.js
+++ src/components/app.js
@@ -1,37 +1,35 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import { bindActionCreators } from 'redux'
 import DynamicForm from './dynamic-form'
 import { model } from '../utils/mocks/form'
 import * as appActs from '../state/modules/form/actions'
 
-class App extends Component {
-  componentDidMount = () => {
-    const { appActions } = this.props
+const handleSubmit = model => {
+  console.log(JSON.stringify(model))
+}
+
+const App = props => {
+  useEffect(() => {
+    const { appActions } = props
     appActions && appActions.addForm({ name: 'model', data: model })
+  }, [])
+
+  const { models } = props
+  if (!models || !models.model) {
+    return (
+      <div>Loading...</div>
+    )
   }
 
-  handleSubmit = model => {
-    console.log(JSON.stringify(model))
-  }
-
-  render = () => {
-    const { models } = this.props
-    if (models && models.model) {
-      return (
-        <DynamicForm
-          title='testing'
-          model={models.model}
-          modelName='model'
-          handleSubmit={model => this.handleSubmit(model)}
-        />
-      )
-    } else {
-      return (
-        <div>Loading...</div>
-      )
-    }
-  }
+  return (
+    <DynamicForm
+      title='testing'
+      model={models.model}
+      modelName='model'
+      handleSubmit={model => handleSubmit(model)}
+    />
+  )
 }
 
 const mapStateToProps = reducers => reducers.forms
